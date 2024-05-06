6a55576a42484f1028f34264752dfd697a079eaf
chopancho <pancho.pantoja@outlook.es>
2024-05-06 14:22:43 -0400

    wip: termina prototipo de componente e intento de método post por servicio

--- frontend/src/services/homestay-api-service.service.ts
+++ frontend/src/services/homestay-api-service.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {HomeStayCreateDTO} from "../../../backend/src/homestay/dto"
+import {HomeStayForm} from "../components/add-home-stay/add-home-stay.component";
+import {BehaviorSubject, Observable} from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomestayApiServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  public validateForm(form: HomeStayForm): BehaviorSubject<boolean> {
+    if (
+      form.rooms < 1 ||
+      form.beds < 1 ||
+      form.bathrooms < 1
+    ){
+      const userCreatePost = new HomeStayCreateDTO()
+      userCreatePost.dormitorios = form.rooms
+      userCreatePost.banos = form.bathrooms
+      userCreatePost.camas = form.beds
+      userCreatePost.calle = form.street
+      userCreatePost.descripcion = form.desc
+      userCreatePost.tipo = form.type
+      userCreatePost.fechas_disponibles = [form.initDate, form.finishDate]
+      userCreatePost.comodidades = [form.services]
+      userCreatePost.reglas = [form.rules]
+      userCreatePost.opciones_de_seguridad = [form.securityOptions!, "otra opción"]
+      userCreatePost.opciones_de_llegada = [form.arrivalOptions!, "otra opción"]
+      if (form.streetNumber) userCreatePost.numero_casa = form.streetNumber
+      if (form.depNumber) userCreatePost.numero_dpto = form.depNumber
+
+      userCreatePost.ownerId = ( Math.random() * 100 )% 1
+
+      this.sendForm(userCreatePost)
+
+      return new BehaviorSubject<boolean>(true)
+    }
+    return new BehaviorSubject<boolean>(false)
+  }
+
+  private sendForm(dto: HomeStayCreateDTO) {
+    this.http.post(
+      "",
+      dto
+    )
+  }
 }
