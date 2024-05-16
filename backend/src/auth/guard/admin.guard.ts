371947572f03ab8610da81cf08edd1745dab84e7
chopancho <pancho.pantoja@outlook.es>
2024-05-16 12:00:46 -0400

    wip: remplazo de backend

--- backend/src/auth/guard/admin.guard.ts
+++
@@ -1,7 +0,0 @@
-import { AuthGuard } from '@nestjs/passport';
-
-export class AdminJwtGuard extends AuthGuard('adminjwt') {
-  constructor() {
-    super();
-  }
-}
--- backend/src/user/dto/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './userRegister.dto';
-export * from './userCreate.dto';
-export * from './userUpdate.dto';
